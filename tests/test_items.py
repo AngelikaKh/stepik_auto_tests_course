39d96e61ed7e58e5dc2a4dfb54184285b97a5bcd
AngelikaKh <a.khodzhaian@gmail.com>
2023-06-04 17:23:33 +0400

    refactoring

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,12 +1,10 @@
 from selenium.webdriver.common.by import By
-from selenium import webdriver
 import time
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207//"
 
 
 def test_add_to_basket_button_is_displayed(browser):
-    browser = webdriver.Chrome()
     browser.get(link)
     # time.sleep(5)
     button = browser.find_elements(By.CSS_SELECTOR, "#add_to_basket_form > button")
