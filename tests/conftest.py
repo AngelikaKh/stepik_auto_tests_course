aa8afcb2153fa0878ddee2369543b1d1acd73ecc
AngelikaKh <a.khodzhaian@gmail.com>
2023-06-04 17:13:33 +0400

    test for different languages

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,45 +1,31 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as OptionsChrome
-from selenium.webdriver.firefox.options import Options as OptionsFirefox
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--browser_name', action='store', default="Chrome",
+                     help="Выберите браузер: Chrome или Firefox")
+    parser.addoption('--language', action='store', default="en",
+                     help="Выберите язык: ru, en or fr")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+    user_language = request.config.getoption("language")
+    if browser_name == "Chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        print("\nЗапуск браузера Chrome для теста..")
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "Firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        print("\nЗапуск браузера Firefox для теста..")
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+        raise pytest.UsageError("--browser_name должно быть Chrome или Firefox")
     yield browser
-    print("\nquit browser..")
+    print("\nЗакрытие браузера..")
     browser.quit()
-
-
-# @pytest.fixture(scope="function")
-# def browser(request):
-# ...
-#     options = OptionsChrome()
-#     options.add_experimental_option(
-#         'prefs', {'intl.accept_languages': user_language})
-#
-#     options_firefox = OptionsFirefox()
-#     options_firefox.set_preference("intl.accept_languages", user_language)
-# ...
-#     if browser_name == "chrome":
-# ...
-#         browser = webdriver.Chrome(options=options)
-#     elif browser_name == "firefox":
-# ...
-#         browser = webdriver.Firefox(options=options_firefox)
-# ...x(options=options)
